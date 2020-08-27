1aabe7b5d23eaaa6bf1f097935e6a462a00e0a2e
v <v@silicolab.bibbox>
2020-08-27 11:43:18 +0200

    test

--- listInstalledApps.py
+++ listInstalledApps.py
@@ -1,11 +1,12 @@
 import os
 import json
 
+os.system('sudo chmod -R 777 conf/')
 with open('conf/usersettings/userinput.json') as json_file:
     data = json.load(json_file)
 
     apps = data['instance']
-print('Your installed apps are')
+    print('Your installed apps are')
     for i in range(len(apps)):
         subdict = apps[i]
         for subkey in subdict:
