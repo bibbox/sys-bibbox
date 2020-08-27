6e805dc29e30a633f07658e8fa05662f4855c6c7
v <v@silicolab.bibbox>
2020-08-27 15:34:30 +0200

    updates

--- removeApp.py
+++ removeApp.py
@@ -4,10 +4,18 @@ import json
 print('Wich app do you want to remove? ')
 appName = input('Please enter app name: ')
 
-os.system('cd apps/' + appName)
-os.system('docker-compose down')
-os.system('cd ..')
-os.system('cd ..')
+os.system('sudo chmod -R 777 apps')
+
+#os.system('cd apps/' + appName + '/')
+#os.system('docker-compose down')
+#os.system('cd ..')
+#os.system('cd ..')
+
+instApps = os.listdir('apps/'+ appName)
+
+folder = instApps[0]
+
+os.system('docker-compose -f apps/' + appName +'/' +  folder + '/docker-compose-template.yml down ')
 
 os.system('sudo rm -R apps/' + appName)
 
