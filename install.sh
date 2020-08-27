88681fba98be163eca6d4e4431d7eba605945714
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-08-27 14:57:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ source mycommands.sh
 mkdir -p apps
 sudo chmod 777 apps
 
-sudo cp conf/usersettings/userinput.json conf/usersettings/userinputlocal.json
+sudo cp conf/userinput/runningContainers.json conf/userinput/runningContainerslocal.json
