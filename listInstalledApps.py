b5893388d37aa1928e72213f6f27cc5b61227e14
v <v@silicolab.bibbox>
2020-08-27 11:16:28 +0200

    Added listInstalledApps.py

--- listInstalledApps.py
+++ listInstalledApps.py
@@ -1,1 +1,15 @@
-sg
+import os
+import json
+
+with open('conf/usersettings/userinput.json') as json_file:
+    data = json.load(json_file)
+
+    apps = data['instance']
+print('Your installed apps are')
+    for i in range(len(apps)):
+        subdict = apps[i]
+        for subkey in subdict:
+            subvalue = subdict[subkey]
+            print('App name: ' + subvalue)
+            print('Docker Container: ' + subkey)
+            print('------------------------------')
