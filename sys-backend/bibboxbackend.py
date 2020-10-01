1f4de8f7100232b746144a4e9dd09d72870c0b3b
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-01 09:21:32 +0200

    Update bibboxbackend.py

--- sys-backend/bibboxbackend.py
+++ sys-backend/bibboxbackend.py
@@ -608,7 +608,7 @@ class AppController:
             with open(proxyPath + 'template.conf') as template:
                 file_content = template.read()
                 file_content = file_content.replace("§§INSTANCEID", instanceName + '/')
-                file_content = file_content.replace("§§CONTAINERNAME", containerName)
+                file_content = file_content.replace("§§CONTAINERNAME", containerName + '/')
                 template = open( proxyPath + 'proxyconfig/sites/' + name, 'w+')
                 template.write(file_content)
                 template.close()
