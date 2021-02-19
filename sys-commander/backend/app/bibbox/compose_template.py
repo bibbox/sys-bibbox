4058ecf59aeaa8bfe8ff9e4a3a2d464ac6f11a13
Lukas Sch <69352306+Lksman@users.noreply.github.com>
2021-02-19 14:02:09 +0100

    fixed replacement of payload params

--- sys-commander/backend/app/bibbox/compose_template.py
+++ sys-commander/backend/app/bibbox/compose_template.py
@@ -1,49 +1,41 @@
 import yaml
-import re
 
 
 class ComposeTemplate ():
 
-    def __init__(self, template_str, parameters):
+    def __init__(self, template_str, payload):
         self.template_str = template_str
-        self.parameters = parameters
-        self.parameter_dict = {
-                "§§INSTANCE"            : self.parameters[0], 
-                "§§MYSQL_ROOT_PASSWORD" : self.parameters[1], 
-                "§§DATAROOT"            : self.parameters[2]
-            }
+        self.payload = payload
 
     #### todo: add more validators
-    @property
-    def parameters(self):
-        return self._parameters
+    # @property
+    # def template_str(self):
+    #     return self._template_str
 
-    @parameters.setter
-    def parameters(self, parameters):
-        if len(parameters) < 3: raise Exception("Instantiating failed. Too few parameters. Expected 3")
-        if len(parameters) > 3: raise Exception("Instantiating failed. Too many parameters. Expected 3")
-        self._parameters = parameters
+    # @template_str.setter
+    # def template_str(self, template_str):
+    #     if not len(template_str): raise Exception("Instantiating failed. Empty Template String.")
+    #     self._template_str = template_str
 
     
     def getCompose (self):
 
-        temp_file_str = self.__replacePlaceholders(self.parameter_dict)
-
+        compose_str = self.__replacePlaceholders(self.payload)
         keys_to_remove = [
              "proxy", 
              "ports"
             ]
 
-        temp_file_str = self.__removeKeysFromNestedDict(yaml.safe_load(temp_file_str), keys_to_remove)
+        compose_str = self.__removeKeysFromNestedDict(yaml.safe_load(compose_str), keys_to_remove)
 
-        return temp_file_str
+        return compose_str
 
     def getComposeLocal (self):
 
-        modified_parameter_dict = self.parameter_dict.copy()
-        modified_parameter_dict['§§INSTANCE'] = 'bibbox'
+        modified_payload = self.payload
+        modified_payload['instancename'] = 'bibbox'
 
-        temp_file_str = self.__replacePlaceholders(modified_parameter_dict)
+        compose_local_str = self.__replacePlaceholders(modified_payload)
 
         keys_to_remove = [
              "proxy", 
@@ -51,20 +43,20 @@ class ComposeTemplate ():
              "networks"
             ]
 
-        temp_file_str = self.__removeKeysFromNestedDict(yaml.safe_load(temp_file_str), keys_to_remove)
+        compose_local_str = self.__removeKeysFromNestedDict(yaml.safe_load(compose_local_str), keys_to_remove)
         
-        return temp_file_str
+        return compose_local_str
     
     def getProxyInformation (self):
 
-        temp_file_str = self.__replacePlaceholders(self.parameter_dict)
-
+        compose_str = self.__replacePlaceholders(self.payload)
         proxy_info = []
-        services_dict = yaml.safe_load(temp_file_str)['services']
+        services_dict = yaml.safe_load(compose_str)['services']
+
+
 
         for key in services_dict.keys():
             if 'proxy' in services_dict[key]:
-
                 proxy_entry = {
                     'urlprefix'     : '',
                     'type'          : '',
@@ -72,23 +64,20 @@ class ComposeTemplate ():
                     'displayname'   : '',
                     'container'     : ''
                 }
-
                 port_suffix = services_dict[key]['ports'][0].split(":")[-1]
                 proxy_entry['container'] = "{}:{}".format(services_dict[key]['container_name'], port_suffix)
-
                 for kv_pair in services_dict[key]['proxy']:
                     for key in kv_pair:
                         proxy_entry[key] = kv_pair.get(key)
-
                 proxy_info.append(proxy_entry)
 
         return proxy_info
             
     def getContainerNames (self):
-
-        temp_file_str = self.__replacePlaceholders(self.parameter_dict)
+        compose_str = self.__replacePlaceholders(self.payload)
         container_names = []
-        services_dict = yaml.safe_load(temp_file_str)['services']
+        
+        services_dict = yaml.safe_load(compose_str)['services']
 
         for key in services_dict.keys():
             if 'container_name' in services_dict[key]:
@@ -96,23 +85,35 @@ class ComposeTemplate ():
 
         return container_names
 
-    def __removeKeysFromNestedDict(self, passed_dict, keys_to_remove):
-        dict_temp = passed_dict.copy()
+    def __removeKeysFromNestedDict(self, compose_dict, keys_to_remove):
+        dict_temp = compose_dict.copy()
 
         for key in dict_temp.keys():
             if isinstance(dict_temp[key], dict):
-                self.__removeKeysFromNestedDict(passed_dict[key], keys_to_remove)
+                self.__removeKeysFromNestedDict(compose_dict[key], keys_to_remove)
 
             if key in keys_to_remove:
                 try:
-                    del passed_dict[key]
+                    del compose_dict[key]
                 except KeyError as exc:
                     print(exc)
 
-        return passed_dict
+        return compose_dict
 
-    def __replacePlaceholders(self, passed_dict):
-        return re.sub(r'§§[A-Z_]+', lambda match: passed_dict.get(match.group()), self.template_str)
+    def __replacePlaceholders(self, compose_dict):
+        str = self.template_str
+        str = str.replace('§§INSTANCE', compose_dict['instancename'])
+        str = str.replace('§§DATAROOT', compose_dict['dataroot'][:-1]) # [:-1] to remove trailing /
+        
+        # only applicable if §§KEY in docker-compose-template.yml == KEY in payload['payload'] (without §§ Prefix) 
+        # for key, value in self.payload.items():
+        #     if not isinstance(value, dict):
+        #         str = str.replace('§§' + key, value)
+
+        for key, value in self.payload['payload'].items():
+            str = str.replace('§§' + key, value)
+
+        return str
 
 ### testing 
 import os 
@@ -121,10 +122,27 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 with open(dir_path + "/test_output/docker-compose-template-testing.yml", 'r') as template_obj:
     template_str = template_obj.read()
-    compose_class_instance = ComposeTemplate(template_str, ["test-instance-Name", "test_pw", "/opt/test/directory"])
 
+    payload = {
+        "appname"       : "app-wordpress",
+        "instancename"  : "app-wordpress-instance", #id
+        "version"       : "V4",
+        "displayname"   : "Wordpress Test",
+        "dataroot"      : "/opt/bibbox/instance-data/",
+        "payload"       : 
+                    {
+                        "MYSQL_ROOT_PASSWORD" : "quaksi"
+                    }            
+    }
+
+    compose_class_instance = ComposeTemplate(template_str, payload)
+    # compose_class_instance = ComposeTemplate(template_str, ["test-instance-Name", "/opt/test/directory", "test_pw"])
 
 repeat = 50
+
+print("\n{} {} {}\n".format("#"*repeat, "COMPOSE TEMPLATE ", "#"*repeat))
+print(yaml.dump(yaml.safe_load(compose_class_instance.template_str), default_flow_style=False))
+
 print("\n{} {} {}\n".format("#"*repeat, "COMPOSE", "#"*repeat))
 print(yaml.dump(compose_class_instance.getCompose(), default_flow_style=False))
 
