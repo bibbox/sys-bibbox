48fe3653a705a07f98daf6b3224edd823818fb0d
Lukas Sch <69352306+Lksman@users.noreply.github.com>
2021-03-23 12:04:45 +0100

    added file log handlers

--- sys-commander/backend/app/services/db_logger_service.py
+++ sys-commander/backend/app/services/db_logger_service.py
@@ -1,14 +1,17 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
 from backend.app.models.log import Log
 from backend.app import db
 
+
+# A very basic logger-handler that commits a LogRecord to the SQL Db
 class DBLogsHandler(logging.Handler):
     def __init__(self, activity_id):
         super(DBLogsHandler, self).__init__()
         self.activity_id = activity_id
 
-    # A very basic logger that commits a LogRecord to the SQL Db
     def emit(self, record):
         trace = None
         exc = record.__dict__['exc_info']
@@ -35,7 +38,36 @@ class DBLoggerService():
         return self.logger
 
     def _configLogger(self):
+        MAX_BYTES = 100000 # Maximum size for a log file
+        BACKUP_COUNT = 3 # Maximum number of old log files
+        log_format = logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s')
+
+        # TODO: Make relative path to file logs cleaner
+
+        debug_file_handler = RotatingFileHandler(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../logs/debug.log'), maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT)
+        debug_file_handler.setFormatter(log_format)
+        debug_file_handler.setLevel(logging.DEBUG)
+
+        info_file_handler = RotatingFileHandler(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../logs/info.log'), maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT)
+        info_file_handler.setFormatter(log_format)
+        info_file_handler.setLevel(logging.INFO)
+
+        warn_file_handler = RotatingFileHandler(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../logs/warning.log'), maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT)
+        warn_file_handler.setFormatter(log_format)
+        warn_file_handler.setLevel(logging.WARNING)
+        
+        error_file_handler = RotatingFileHandler(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../logs/error.log'), maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT)
+        error_file_handler.setFormatter(log_format)
+        error_file_handler.setLevel(logging.ERROR)
+
+        db_handler = DBLogsHandler(self.activity_id)
+        db_handler.setLevel(logging.INFO)
+
         logger = logging.getLogger(self.logger_name)
-        logger.setLevel(logging.INFO)
-        logger.addHandler(DBLogsHandler(self.activity_id))
+        logger.addHandler(debug_file_handler)
+        logger.addHandler(info_file_handler)
+        logger.addHandler(warn_file_handler)
+        logger.addHandler(error_file_handler)
+        logger.addHandler(db_handler)
+
         return logger
