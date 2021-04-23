9c2e352605290b7c9f1403872522fff417ae6256
Lukas Sch <69352306+Lksman@users.noreply.github.com>
2021-04-23 11:53:13 +0200

    install v1.3

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,6 +3,7 @@
 TODO before running this script:
 cd /opt
 sudo mkdir bibbox
+sudo apt install git -y
 
 git clone https://github.com/bibbox/sys-bibbox.git
 cd sys-bibbox
@@ -16,7 +17,7 @@ sudo apt install git -y
 sudo apt install python3-pip -y
 sudo apt-get install python3-venv -y
 sudo npm i -g @angular/cli -n
-ng analytics off
+export NG_CLI_ANALYTICS="false"
 
 python3 -m pip install virtualenv
 
@@ -33,17 +34,25 @@ mkdir config
 
 python3 -m venv bibbox-venv
 source bibbox-venv/bin/activate
-cd /opt/bibbox/sys-bibbox/backend
+cd backend
 pip3 install -r requirements.txt
 pip3 install docker-compose
 
-cd /opt/bibbox/sys-bibbox/frontend
+cd ../frontend
+
+ng analytics off
 sudo npm i
+sudo ng build --prod
 
 cp /opt/bibbox/sys-bibbox/config-templates/100-error.conf /opt/bibbox/proxy/sites/100-error.conf
 cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf /opt/bibbox/proxy/sites/000-default.conf
 cp /opt/bibbox/sys-bibbox/config-templates/bibbox.config /opt/bibbox/config/bibbox.config
 cp /opt/bibbox/sys-bibbox/config-templates/proxy-default.template /opt/bibbox/config/proxy-default.template
 
-sudo `which docker-compose` up --build
-sudo docker restart bibbox-sys-commander-backend
+sudo `which docker-compose` build
+sudo `which docker-compose` up --no-start
+docker start bibbox-sys-commander-backend bibbox-sys-commander-postgres
+docker exec -it bibbox-sys-commander-backend python manage.py recreate_db
+
+sudo `which docker-compose` stop
+sudo `which docker-compose` up
