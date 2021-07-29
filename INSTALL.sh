f0413a4b5225ba81a095ede359c830d94cc8ff69
Lukas Sch <69352306+Lksman@users.noreply.github.com>
2021-07-29 18:22:47 +0200

    included cp websocket template in INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -85,7 +85,9 @@ ng build --prod
 cp /opt/bibbox/sys-bibbox/config-templates/100-error.conf /opt/bibbox/proxy/sites/100-error.conf
 cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf /opt/bibbox/proxy/sites/000-default.conf
 cp /opt/bibbox/sys-bibbox/config-templates/bibbox.config /opt/bibbox/config/bibbox.config
+
 cp /opt/bibbox/sys-bibbox/config-templates/proxy-default.template /opt/bibbox/config/proxy-default.template
+cp /opt/bibbox/sys-bibbox/config-templates/proxy-websocket.template /opt/bibbox/config/proxy-websocket.template
 
 
 docker-compose up --build -d
