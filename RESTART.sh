7d2485320e43b334836981c5c92b40e592f4519f
Emilian Jungwirth <emilian.jung+github@gmail.com>
2022-02-03 17:40:24 +0100

    restart bibbox

---
+++ RESTART.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker-compose down
+
+cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf.apache2 /opt/bibbox/proxy/sites/000-default.conf
+
+docker-compose up --build -d
+
+docker exec -it bibbox-sys-commander-apacheproxy certbot --apache -d ${DOMAINNAME:-demo.bibbox.org} -n --email ${EMAIL:-backoffice.bibbox@gmail.com} --agree-tos
+
+cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf /opt/bibbox/proxy/sites/000-default-le-ssl.conf
+
+#docker exec -it bibbox-sys-commander-apacheproxy cp /usr/local/apache2/conf/sites/000-default.conf /etc/apache2/sites-available/000-default-le-ssl.conf
+
+docker exec -it bibbox-sys-commander-apacheproxy apache2ctl -k graceful
