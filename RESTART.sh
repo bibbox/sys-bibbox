4498079dfdbb0ea61dc8400a032244ab8303c8ce
Emilian Jungwirth <emilian.jung+github@gmail.com>
2022-02-08 13:30:45 +0100

    link sites-enabled for apps

--- RESTART.sh
+++ RESTART.sh
@@ -9,6 +9,12 @@ docker-compose up --build -d
 
 docker exec bibbox-sys-commander-apacheproxy ln -s ../sites-available/000-default.conf /etc/apache2/sites-enabled/
 
+for proxy_conf_file in /opt/bibbox/proxy/sites/005*
+do
+  name=$(basename $proxy_conf_file)
+  docker exec bibbox-sys-commander-apacheproxy ln -s ../sites-available/${name} /etc/apache2/sites-enabled/
+done
+
 docker exec -it bibbox-sys-commander-apacheproxy certbot --apache -d ${DOMAINNAME:-demo.bibbox.org} -n --email ${EMAIL:-backoffice.bibbox@gmail.com} --agree-tos
 
 cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf /opt/bibbox/proxy/sites/000-default-le-ssl.conf
