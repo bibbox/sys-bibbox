56a6b9952b66678539225cf3333325e535f69895
Emilian Jungwirth <emilian.jung+github@gmail.com>
2022-02-08 09:34:48 +0100

    updated INSTALL.sh and RESTART.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -154,6 +154,8 @@ cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf /opt/bibbox/proxy/si
 docker exec -it bibbox-sys-commander-apacheproxy apache2ctl -k graceful
 
 
+docker exec bibbox-sys-commander-apacheproxy ln -s ../sites-available/000-default.conf /etc/apache2/sites-enabled/
+
 docker exec -it bibbox-sys-commander-apacheproxy certbot --apache -d ${DOMAINNAME:-demo.bibbox.org} -n --email ${EMAIL:-backoffice.bibbox@gmail.com} --agree-tos
 
 cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf /opt/bibbox/proxy/sites/000-default-le-ssl.conf
