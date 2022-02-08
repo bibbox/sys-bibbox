3de8caeace6d3f51f3786b7fbffc22702e3b7fce
Emilian Jungwirth <emilian.jung+github@gmail.com>
2022-02-08 13:54:01 +0100

    add proxyies for instances

--- RESTART.sh
+++ RESTART.sh
@@ -9,15 +9,17 @@ docker-compose up --build -d
 
 docker exec bibbox-sys-commander-apacheproxy ln -s ../sites-available/000-default.conf /etc/apache2/sites-enabled/
 
-for proxy_conf_file in /opt/bibbox/proxy/sites/005*
-do
-  name=$(basename $proxy_conf_file)
-  docker exec bibbox-sys-commander-apacheproxy ln -s ../sites-available/${name} /etc/apache2/sites-enabled/
-done
 
 docker exec -it bibbox-sys-commander-apacheproxy certbot --apache -d ${DOMAINNAME:-demo.bibbox.org} -n --email ${EMAIL:-backoffice.bibbox@gmail.com} --agree-tos
 
 cp /opt/bibbox/sys-bibbox/config-templates/000-default.conf /opt/bibbox/proxy/sites/000-default-le-ssl.conf
+
+for proxy_conf_file in /opt/bibbox/proxy/sites/005*
+do
+  name=$(basename $proxy_conf_file)
+  docker exec bibbox-sys-commander-apacheproxy ln -s ../sites-available/${name} /etc/apache2/sites-enabled/
+  docker exec bibbox-sys-commander-apacheproxy certbot --expand --apache -d ${name}.${DOMAINNAME:-demo.bibbox.org} -n --email ${EMAIL:-backoffice.bibbox@gmail.com} --agree-tos
+done
 
 #docker exec -it bibbox-sys-commander-apacheproxy cp /usr/local/apache2/conf/sites/000-default.conf /etc/apache2/sites-available/000-default-le-ssl.conf
 
