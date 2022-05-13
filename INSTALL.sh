a14a1dad78006a56af758d93a3a71cb46cdf054f
Emilian Jungwirth <emilian.jung+github@gmail.com>
2022-05-13 11:42:26 +0200

    add create bibbox netword

--- INSTALL.sh
+++ INSTALL.sh
@@ -102,6 +102,7 @@ cp /opt/bibbox/sys-bibbox/config-templates/005-fdp.conf /opt/bibbox/proxy/sites/
 cp /opt/bibbox/sys-bibbox/config-templates/proxy-default.template /opt/bibbox/config/proxy-default.template
 cp /opt/bibbox/sys-bibbox/config-templates/proxy-websocket.template /opt/bibbox/config/proxy-websocket.template
 
+docker network create bibbox-default-network
 
 docker-compose up --build -d
 
