19a97f84d4957c073cd69fb209d8e409602ec5ff
AlexMartazan <118280106+AlexMartazan@users.noreply.github.com>
2023-07-27 10:50:54 +0200

    Added keycloak-check script

---
+++ backend/keycloak-check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Function to check if the URL gives a 200 status
+check_url() {
+    status_code=$(curl --head --write-out '%{http_code}' --silent --output /dev/null -fsS http://bibbox-sys-commander-keycloak:8080/auth/realms/master)
+    if [ "$status_code" -eq 200 ]; then
+        return 1
+    else
+        return 0
+        
+    fi
+}
+
+# Wait until the URL gives a 200 status
+while check_url; do
+    echo "Waiting for keycloak..."
+    sleep 5
+done
+
+exec /usr/src/bibbox-sys-commander/entrypoint.sh
