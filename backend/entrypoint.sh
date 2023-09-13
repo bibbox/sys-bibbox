0d2bdd57ba0e6b9094f5387b95398a473601d088
Emilian Jungwirth <emilian.jung+github@gmail.com>
2023-09-13 16:27:27 +0200

    backend fixes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Below shells script is required because the flask container need to wait for postgres db server to startup before
 # accessing it below.
@@ -11,14 +11,19 @@ USER=postgres
 DATABASE=bibbox
 HOST=postgres
 
-until psql -h $HOST -U $USER -d $DATABASE -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until PGPASSWORD=postgres psql -h $HOST -U $USER -d $DATABASE -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server to start, $((RETRIES)) remaining attempts..."
   RETRIES=$((RETRIES-=1))
   sleep 1
 done
 
 echo "PostgreSQL started!"
-
+RETRIES=15
+until $(curl --output /dev/null --silent --head --fail $KEYCLOAK_SERVER_URL) || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for keycloak server to start, $((RETRIES)) remaining attempts..."
+  RETRIES=$((RETRIES-=1))
+  sleep 5
+done
 
 # Run below commands from manage.py to initialize db and have some default data.
 # add a flag to preserve the DB at a build
@@ -32,7 +37,9 @@ fi
 # test if the db is working
 # python manage.py
 
-
+if [[ ! -e '/var/log/uwsgi/' ]]; then
+    mkdir '/var/log/uwsgi/'
+fi
 
 uwsgi --ini /etc/uwsgi.ini
 
