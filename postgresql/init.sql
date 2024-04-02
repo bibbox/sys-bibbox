4a6cc9799ba4e37d61a652746542a74eb57c9990
AlexMartazan <alexandru.martazan@medunigraz.at>
2024-04-02 12:53:10 +0000

    Allow user keycloak to change tables

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -5,3 +5,5 @@ CREATE DATABASE bibbox;
 CREATE DATABASE keycloak;
 CREATE USER keycloak WITH ENCRYPTED PASSWORD 'keycloak';
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+\c keycloak postgres
+GRANT ALL ON SCHEMA public TO keycloak;
